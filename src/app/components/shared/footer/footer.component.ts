423cc646c962342c0842e4c674c9adc20a7c7dc2
Arvind Kumar <arvind2y@gmail.com>
2018-10-26 19:21:17 +0530

    Contact-mgr-master basic layout

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -6,11 +6,17 @@ import { Component, OnInit } from '@angular/core';
   <footer class="text-center bg-light">
     <hr>
     <p>Contact Manager App built in Training in 2018</p>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
     <app-nav>
       <li class="nav-item">
         <a class="nav-link" href="#">Goto Top</a>
       </li>
     </app-nav>
+    </ul>
+    </div>
+    </nav>
     <p class="red">Copyright &copy; 2018</p>
   </footer>
   `,
